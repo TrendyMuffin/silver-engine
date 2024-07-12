114359c9d77b483720668b3f49ce7bc4c5603791
TrendyMuffin <Nu8th.store@gmail.com>
2024-07-12 14:40:49 +0300

    Update FormSubmissionHandler.js

--- FormSubmissionHandler.js
+++ FormSubmissionHandler.js
@@ -1,28 +1,30 @@
-            document.getElementById('orderForm').addEventListener('submit', function (event) {
-                event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('orderForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-                const formData = new FormData(this);
-                const data = {};
-                formData.forEach((value, key) => {
-                    data[key] = value;
-                });
-
-                const webhookUrl = 'https://hook.eu2.make.com/vs1aclwvua13mc4dag5liyx565d5cld6';
-
-                fetch(webhookUrl, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                })
-                    .then(response => response.json())
-                    .then(result => {
-                        console.log('Success:', result);
-                        openModal(document.getElementById('Submit message'));
-                    })
-                    .catch(error => {
-                        console.error('Error:', error);
-                    });
-            });
+        const formData = new FormData(this);
+        const data = {};
+        formData.forEach((value, key) => {
+            data[key] = value;
         });
+
+        const webhookUrl = 'https://hook.eu2.make.com/vx7vtirrifw841m8eqxbxx1wyu7bj16c';
+
+        fetch(webhookUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log('Success:', result);
+            const modal = document.getElementById('Submit message');
+            modal.classList.add('is-active');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+});
